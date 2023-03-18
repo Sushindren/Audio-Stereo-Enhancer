89bc0317243327b15983083f270c9a9e3e74f9df
Sushindren <83157225+Sushindren@users.noreply.github.com>
2023-03-18 18:22:34 +0530

    Update StereoEnhancement.c

--- StereoEnhancement.c
+++ StereoEnhancement.c
@@ -13,3 +13,15 @@ void StereoEnhance(float *src, float gain)
 		src[i + 1] = (src[i + 1] > 1.0f) ? 1.0f : ((src[i + 1] < -1.0f) ? -1.0f : src[i + 1]);
 	}
 }
+
+void MonoEnhance(float *src,float gain)
+{
+    for (int i = 0; i < FRAME; i += 2)
+	{
+		float mid = src[i];
+		src[i] = (mid * gain)/2;
+		src[i + 1] = (mid * gain)/2;
+		src[i] = (src[i] > 1.0f) ? 1.0f : ((src[i] < -1.0f) ? -1.0f : src[i]);
+		src[i + 1] = (src[i + 1] > 1.0f) ? 1.0f : ((src[i + 1] < -1.0f) ? -1.0f : src[i + 1]);
+	}
+}
